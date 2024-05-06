82e7a8a36f933b58ead962549768a70954fc8c0b
Peculiars <olaitandamilare230@gmail.com>
2024-05-06 16:11:24 +0100

    implementing the header and the sidebar

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+import CountUp from 'react-countup';
+
+const AnimatedCounter = ({amount}:{amount: number}) => {
+  return (
+    <div>
+        <CountUp
+        duration={2.5}
+        decimal='.'
+        decimals={2}
+        prefix='$'
+        end={amount}/>
+    </div>
+  )
+}
+
+export default AnimatedCounter
