ac519646189bea3402c8466f7e3c2b014cad294d
Peculiars <olaitandamilare230@gmail.com>
2024-05-12 00:16:47 +0100

    implementing plaid

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './ui/button'
+import {PlaidLinkOptions, PlaidLinkOnSuccess, usePlaidLink} from 'react-plaid-link'
+import { useRouter } from 'next/navigation';
+import { createLinkToken } from '@/lib/actions/user.action';
 
 const PlaidLink = ({user, variant}:PlaidLinkProps) => {
+
+    const router = useRouter()
+
+    const [token, setToken] = useState('');
+
+    useEffect(()=>{
+        const getLinkedToken = async()=>{
+            const data = await createLinkToken(user)
+            setToken(data?.linkToken)
+        }
+
+        getLinkedToken()
+    },[user])
+
+    const onSuccess = useCallback<PlaidLinkOnSuccess>(async(public_token : string)=>{
+        // await exchangePublicToken({
+        //     publicToken: public_token,
+        //     user,
+        // })
+        router.push('/');
+    },[user])
+
+    const config:PlaidLinkOptions = {
+        token,
+        onSuccess
+    }
+    const {open, ready} = usePlaidLink(config);
   return (
     <>
     { variant === 'primary' ? (
-        <Button className='plaidlink-primary'>Connect Bank</Button>
+        <Button onClick={()=> open()} disabled={!ready} className='plaidlink-primary'>Connect Bank</Button>
     ) : variant === 'ghost' ? (
         <Button>Connect Bank</Button>
     ) : (
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -4,6 +4,8 @@ import { ID } from "node-appwrite"
 import { createAdminClient, createSessionClient } from "../appwrite"
 import { cookies } from "next/headers"
 import { parseStringify } from "../utils"
+import { CountryCode, ProcessorTokenCreateRequest, ProcessorTokenCreateRequestProcessorEnum, Products } from "plaid"
+import { plaidClient } from "../plaid"
 
 export const signIn = async ({email, password}: signInProps)=>{
     try {
@@ -58,6 +60,56 @@ export async function getLoggedInUser() {
         await account.deleteSession('current');
     } catch (error) {
         return null
+    }
+  }
+
+  export const createLinkToken = async(user:User)=>{
+    try {
+        const tokenParams ={
+            user :{
+                client_user_id: user.$id
+            },
+            client_name: user.name,
+            products: ['auth'] as Products[],
+            language: 'en',
+            country_codes: ['US'] as CountryCode[],
+        }
+        const response = await plaidClient.linkTokenCreate(tokenParams);
+
+        return parseStringify({linkToken: response.data.link_token})
+    } catch (error) {
+        console.log(error);
+        
+    }
+  }
+
+  export const exchangePublicToken = async({publicToken, user}: exchangePublicTokenProps) =>{
+    try {
+        const response = await plaidClient.itemPublicTokenExchange({public_token: publicToken});
+        const accessToken = response.data.access_token;
+        const itemId = response.data.item_id;
+
+        const accountsResponse = await plaidClient.accountsGet({access_token:accessToken})
+
+        const accountData = accountsResponse.data.accounts[0];
+
+        const request: ProcessorTokenCreateRequest = {
+            access_token: accessToken,
+            account_id: accountData.account_id,
+            processor: 'dwolla' as ProcessorTokenCreateRequestProcessorEnum,
+        };
+
+        const processorTokenResponse = await plaidClient.processorTokenCreate(request);
+        const processorToken = processorTokenResponse.data.processor_token;
+
+        const fundingSourceUrl = await addFundingsource({
+            dwollaCustomerId: user.dwollaCustomerId,
+            processorToken,
+            bankName: accountData.name,
+        })
+    } catch (error) {
+        console.error('An error occurred while creating exchange token:', error);
+        
     }
   }
   
